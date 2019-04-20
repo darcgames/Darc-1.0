063d6ef1dd63ab6423ac0704f41d05178c22d0a9
Victor <vicojeje25@gmail.com>
2019-04-19 23:30:45 -0700

    added a new scene and script for coin animation

---
+++ demo_game/SCENES/Coin_Collision.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _ready():
+	$Coin_collision.play('spin')
+	
+
+
+func _on_Coin_Collision_body_entered(body):
+	if body.get_name() == 'player':
+		queue_free()
--- demo_game/SCRIPTS/player_motion.gd
+++ demo_game/SCRIPTS/player_motion.gd
@@ -1,45 +1,45 @@
 extends KinematicBody2D
 
-export var speed = 300
-export var gravity = 10
+export var SPEED = 300
+export var GRAVITY = 10
 var motion = Vector2()
 
 # TELLS THE SCENE WHAT DIRECTION IS UP, SO THE PLAYER CAN JUMP
 const UP = Vector2(0, -1)
 
-var jump = 400
+export var JUMP_HEIGHT = 400
 
 func _physics_process(delta):
 	# ADDS GRAVITY TO THE PLAYER TO MAKE HIM FALL
-	motion.y += gravity
+	motion.y += GRAVITY
 	
 	# HANDLES WHAT ACTION WILL OCCUR WHEN THE LEFT ARROW KEY IS PUSHED
 	if Input.is_action_pressed('ui_left'):
-		motion.x = -speed
-		$Sprite.flip_h = true
-		$Sprite.play("run")
+		motion.x = -SPEED
+		$player_sprite.flip_h = true
+		$player_sprite.play("run")
 	
 	# HANDLES WHAT ACTION WILL OCCUR WHEN THE RIGHT ARROW KEY IS PUSHED
 	elif Input.is_action_pressed('ui_right'):
-		motion.x = speed
-		$Sprite.flip_h = false
-		$Sprite.play("run")
-		
+		motion.x = SPEED
+		$player_sprite.flip_h = false
+		$player_sprite.play("run")
+	
 	# HANDLES WHAT ACTION WILL OCCUR WHEN NO KEY IS PUSHED
 	else:
 		motion.x = 0
-		$Sprite.play("idle")
+		$player_sprite.play("idle")
 	
 	# HANDLES WHAT HAPPENS WHEN THE PLAYER IS ON THE FLOOR
 	if is_on_floor():
 		
 		# HANDLES WHAT HAPPENS WHEN THE PLAYER PUSHES THE UP BUTTON 
 		if Input.is_action_just_pressed('ui_up'):
-			motion.y = -jump
-			
-	# HANDLES WHAT HAPPENS WHEN THE PLAYER IS IN THE AIR
+			motion.y = -JUMP_HEIGHT
 	else:
-		$Sprite.play("land")
+		$player_sprite.play('jump')
+			
+	
 	
 	motion = move_and_slide(motion, UP)
 	
